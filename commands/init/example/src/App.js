42d5216a850e359f7457724ca56f504fd6a98278
Patrick Sachs <kontakt@patrick-sachs.de>
2018-09-03 01:44:16 +0200

    Added quick usage hint to example

--- commands/init/example/src/App.js
+++ commands/init/example/src/App.js
@@ -8,10 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React &amp; create-react-protoype</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <br/>
+          Since this example uses the <code>/dist</code> output of your app, make sure to have <code>npm run watch</code> running alongside.
         </p>
       </div>
     );
