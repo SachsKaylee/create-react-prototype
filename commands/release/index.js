680375138e490b2b05a3712fbd54cdb395cfcc22
Patrick Sachs <kontakt@patrick-sachs.de>
2018-09-03 02:18:30 +0200

    Implemented release command

--- commands/release/index.js
+++ commands/release/index.js
@@ -1,13 +1,33 @@
+const run = require("../../helper/run");
+const path = require("path");
+const build = require("../build");
+
 const bootstrap = (app) => {
   app
     .command("release", "Publishes your library to NPM! Things are about to get serious!")
     .action((args, callback) => {
-      app.log("released on npm!", args)
-      // todo: read package json and read "private" flag -> if true, abort!
+      process.env.NODE_ENV = "production";
+
+      console.log("📚 Creating a full build before releasing ...");
+      await build.runFullBuild();
+
+      console.log("📚 Releasing your library ...")
+      await runRelease();
+
+      console.log("✨ Success! Your library has released on NPM.");
+
       callback();
     });
 };
 
+const runRelease = async () => {
+  const cwd = process.cwd();
+  const dist = path.join(cwd, "./dist");
+  console.log("Distribution Path:", dist);
+  await run("npm", ["release", dist]);
+};
+
 module.exports = {
-  bootstrap
+  bootstrap,
+  runRelease
 };
