f913674db7efc5f0fcfc5397553f9de9e3289a63
Patrick Sachs <kontakt@patrick-sachs.de>
2018-09-03 04:16:34 +0200

    Add default browserlist to example

--- commands/init/example/src/App.js
+++ commands/init/example/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
           <br/>
-          Since this example uses the <code>/dist</code> output of your app, make sure to have <code>npm run watch</code> running alongside.
+          Since this example uses the <code>/dist</code> output of your library, make sure to have <code>npm run watch</code> running alongside.
         </p>
       </div>
     );
